6f74b3eb12f5f070ac06ff0ef2413b54ba6081d1
diddy48uni <gabriele2.romano@mail.polimi.it>
2023-06-07 00:08:31 +0200

    addwd bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  css: ["bootstrap/dist/css/bootstrap.min.css",
+        "bootstrap-vue-next/dist/bootstrap-vue-next.css"], // add
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <b-alert show variant="primary"><a href="#" class="alert-link">Primary Alert</a></b-alert>
+        <!--b-alert
+      :show="dismissCountDown"
+      dismissible
+      variant="warning"
+      @dismissed="dismissCountDown=0"
+      @dismiss-count-down="countDownChanged"
+    >
+      <p>This alert will dismiss after {{ dismissCountDown }} seconds...</p>
+      <b-progress
+        variant="warning"
+        :max="dismissSecs"
+        :value="dismissCountDown"
+        height="4px"
+      ></b-progress>
+    </b-alert>
+    <b-button @click="showAlert" variant="info" class="m-1">
+      Show alert with count-down timer
+    </b-button>
+    </div-->
+    
+  <div class="m-5">
+    <b-button variant="success">Success</b-button>
+    <b-button variant="danger">Danger</b-button>
+    <b-button variant="warning">Warning</b-button>
+
+  </div>
+  <div>
+    <b-card>Hi</b-card>
+  </div>
+
+  <b-accordion class="col-3">
+        <b-accordion-item title="Accordion Item #1" visible>
+          Accordion 1
+        </b-accordion-item>
+        <b-accordion-item title="Accordion Item #2">
+          Accordion 2
+        </b-accordion-item>
+        <b-accordion-item title="Accordion Item #3">
+          Accordion 3
+        </b-accordion-item>
+      </b-accordion>
+</div>
+</template>
+<script>
+/* export default {
+     data() {
+      return {
+        dismissSecs: 10,
+        dismissCountDown: 0,
+        showDismissibleAlert: false
+      }
+    },
+    methods: {
+      countDownChanged(dismissCountDown) {
+        this.dismissCountDown = dismissCountDown
+      },
+      showAlert() {
+        this.dismissCountDown = this.dismissSecs
+      }
+    }
+  } */
+</script>
