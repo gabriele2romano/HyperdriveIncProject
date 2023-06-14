0e13170a52e6f7f679d9383a014ace3a99a69a69
ZitoKadabra <eliazito21@gmail.com>
2023-06-14 19:26:49 +0200

    ugly navbar

--- app.vue
+++ app.vue
@@ -1,6 +1,61 @@
 <template>
+  <header>
+
+    <b-navbar>
+        <b-nav-item><NuxtLink to="/">Home</NuxtLink></b-nav-item>
+        <b-nav-item><NuxtLink to="/about">About</NuxtLink></b-nav-item>
+        <b-nav-item><NuxtLink to="/people">People</NuxtLink></b-nav-item>
+      </b-navbar>
+ 
+  </header>
+  <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar">
+        </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+
   <div>
     <!--NuxtWelcome /-->
+
+ 
     <NuxtPage/>
   </div>
 </template>
