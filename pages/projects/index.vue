9f545e78e487c176907e752ee9d9efea0caa9ce2
gabriele2romano <gabriele2.romano@mail.polimi.it>
2023-06-29 14:28:54 +0200

    Update index.vue

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -26,16 +26,16 @@ const filterByArea = (area_id) => {
     }
 }
 
-var selected_area = 0
+var selected_area = ref(0)
 var area_id = ref(0)
 const forceRender = () => {
-    area_id.value = selected_area
+    area_id.value = selected_area.value
     //console.log("LID: "+area_id.value)
-    filtered_projects.value = filterByArea(selected_area)
+    filtered_projects.value = filterByArea(selected_area.value)
     //console.log("Projects: "+JSON.stringify(filtered_projects.value))
 }
 const resetFilter = () => {
-    selected_area = 0
+    selected_area.value = 0
     filtered_projects.value = projects.value
 }
 
@@ -69,6 +69,7 @@ const resetFilter = () => {
             <v-divider cols="12" class="my-6"></v-divider>
             <!-- Start Projects -->
             <v-row>
+                <v-col v-if="filtered_projects.length==0" ><div  class="text-h6 font-weight-thin ">No Projects with selected area</div></v-col>
                 <v-col cols="12" md="4" sm="6" v-for="project in filtered_projects" :key="filtered_projects">
                     <ProjectCard :key="filtered_projects" :project_id=project.id :project_title=project.title :project_overview=project.problem :project_image=project.images[0]></ProjectCard>
                 </v-col> 
